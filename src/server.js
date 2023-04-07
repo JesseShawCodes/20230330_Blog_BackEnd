d020516bcfc2deb6e6c916e02fa06d060c5344a6
Jesse Shaw <JesseShawCodes@gmail.com>
2023-04-07 18:23:31 -0400

    Server.js updates - Auth related updates

--- src/server.js
+++ src/server.js
@@ -17,44 +17,44 @@ const app = express();
 app.use(express.json())
 
 app.use(async (req, res, next) => {
-    const { authToken } = req.headers;
+    const { authtoken } = req.headers;
 
-    if (authToken) {
+
+    if (authtoken) {
         try {
-            req.user = await admin.auth().verifyIdToken(authToken);
+            req.user = await admin.auth().verifyIdToken(authtoken);
         } catch (e) {
-            res.sendStatus(400)
+            return res.sendStatus(400)
         }
     }
+
+    
+    req.user = req.user || {};
+    
 
     next();
 })
 
 app.get("/api/articles/:name", async (req, res) => {
+    console.log("eljlkj")
     const { name } = req.params;
     const { uid } = req.user;
-
+    console.log(`
+        ${req.params.name}
+    `)
 
     const article = await db.collection('articles').findOne({ name });
+    console.log(article)
     if (article) {
         // USer can only upvote article once
         const upvoteIds = article.upvoteIds || [];
         // Make sure user id is not in the id of the article
-        article.canUpvote = uid && !upvoteIds.include(uid);
+        article.canUpvote = uid && !upvoteIds.includes(uid);
         res.json(article);
     }
     else {
-        res.send("ARTICLE NOT FOUND")
+        res.sendStatus(404)
     }
-})
-
-app.post("/hello", (req, res) => {
-    res.send(`Hello ${req.body.name}`);
-});
-
-app.get('/hello/:name/goodbye/:name_2', (req, res) => {
-    const name = req.params
-    res.send(`Hello ${req.params.name}, Goodybe ${req.params.name_2}!`);
 })
 
 app.use((req, res, next) => {
@@ -68,12 +68,13 @@ app.use((req, res, next) => {
 
 app.put("/api/articles/:name/upvote", async (req, res) => {
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
         const upvoteIds = article.upvoteIds || [];
-        const canUpvote = uid && !upvoteIds.include(uid);
+        const canUpvote = uid && !upvoteIds.includes(uid);
 
         if (canUpvote) {
             await db.collection('articles').updateOne({name }, {
@@ -93,8 +94,6 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
 });
 
 app.post("/api/articles/:name/comments", async (req, res) => {
-    console.log("COMMENT RECEIVED!")
-    
     const { name } = req.params;
     const { text } = req.body;
     const { email } = req.user;
@@ -111,7 +110,6 @@ app.post("/api/articles/:name/comments", async (req, res) => {
     else {
         res.send("That article does not exist")
     }
-
 })
 conectToDb(() => {
     console.log("Successfully connected to Database")
