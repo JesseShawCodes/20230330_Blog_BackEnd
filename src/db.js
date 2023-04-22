06a15988a5ebea7dac747061390797819b394e45
Jesse Shaw <JesseShawCodes@gmail.com>
2023-04-22 16:38:05 -0400

    Update to db variables and definitions

--- src/db.js
+++ src/db.js
@@ -7,12 +7,15 @@ dotenv.config()
 let db;
 
 async function conectToDb(cb) {
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.0sk1xrb.mongodb.net/?retryWrites=true&w=majority`);
+    // Local
+    const client = new MongoClient(`${process.env.mongo_client}`)
+
+    // const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.0sk1xrb.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
-    db = client.db('test');
-    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.0sk1xrb.mongodb.net/?retryWrites=true&w=majority/react-blog-db`);
-
+    db = client.db(`${process.env.mongo_db}`);
+    // await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.0sk1xrb.mongodb.net/?retryWrites=true&w=majority/react-blog-db`);
+    await mongoose.connect(`${process.env.mongo_client}/${process.env.mongo_db}`);
     cb();
 }
 
