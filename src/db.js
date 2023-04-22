765c0d29e92638fb8406e8a8821b07af50544c89
Jesse Shaw <JesseShawCodes@gmail.com>
2023-04-22 16:41:28 -0400

    Code Cleanup

--- src/db.js
+++ src/db.js
@@ -7,14 +7,11 @@ dotenv.config()
 let db;
 
 async function conectToDb(cb) {
-    // Local
     const client = new MongoClient(`${process.env.mongo_client}`)
 
-    // const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.0sk1xrb.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
     db = client.db(`${process.env.mongo_db}`);
-    // await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.0sk1xrb.mongodb.net/?retryWrites=true&w=majority/react-blog-db`);
     await mongoose.connect(`${process.env.mongo_client}/${process.env.mongo_db}`);
     cb();
 }
